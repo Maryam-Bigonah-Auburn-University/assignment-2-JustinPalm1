182aa9a476d93a542b38773002d06795734ae2b9
JustinPalm1 <jlp0102@auburn.edu>
2024-09-19 20:00:47 -0500

    Solution q3.cpp

--- Demo/q3.cpp
+++ Demo/q3.cpp
@@ -11,7 +11,25 @@ int main() {
 	    seconds = 0;
 	double kph = 0.0;
 
+// Prompt the user to enter running pace in minutes and seconds
+    cout << "Enter your running pace (minutes and seconds): " << endl;
+    cout << "Minutes: ";
+    cin >> minutes;
+    cout << "Seconds: ";
+    cin >> seconds;
 
+// Convert pace to miles per hour
+    double mphFromPace = convertToMPH(minutes, seconds);
+    cout << fixed << setprecision(2); // Set precision for output
+    cout << "Your pace in miles per hour is: " << mphFromPace << " MPH" << endl;
+
+// Prompt the user to enter speed in kilometers per hour
+    cout << "Enter your speed in kilometers per hour: ";
+    cin >> kph;
+
+// Convert speed to miles per hour
+    double mphFromKPH = convertToMPH(kph);
+    cout << "Your speed in miles per hour is: " << mphFromKPH << " MPH" << endl;
 
 	return 0;
 }
